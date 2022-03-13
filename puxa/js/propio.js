e89c05c121b126f2a1b046879eb2d31a3dc6fac8
serfepe5 <68746993+serfepe5@users.noreply.github.com>
2022-03-13 23:50:29 +0100

    puxa

---
+++ puxa/js/propio.js
@@ -0,0 +1,72 @@
+
+$(document).ready(function(){
+	$("#logojs").hover(function(){
+        $("#logojs").css("height", "100px");
+		$("#logojs").css("width", "200px");
+		}, function(){
+			$("#logojs").css("width", "300px");
+            $("#logojs").css("height", "120px");
+		});
+});
+
+$(document).ready(function(){
+	$("#contacto").hover(function(){
+        $("#contacto").css("font-size", "25px");
+		}, function(){
+			$("#contacto").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#calidad").hover(function(){
+        $("#calidad").css("font-size", "20px");
+		}, function(){
+			$("#calidad").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#reserva").hover(function(){
+        $("#reserva").css("font-size", "25px");
+		}, function(){
+			$("#reserva").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#foto1").hover(function(){
+        $("#foto1").css("opacity","1.0");
+		}, function(){
+			$("#foto1").css("opacity","0.6");
+		});
+});
+
+$(document).ready(function(){
+	$("#inicio").hover(function(){
+        $("#inicio").css("font-size", "25px");
+		}, function(){
+			$("#inicio").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#carta").hover(function(){
+        $("#carta").css("font-size", "25px");
+		}, function(){
+			$("#carta").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#video").hover(function(){
+		$("#video").css("border-style", "solid");
+        $("#video").css("border-color", "chocolate");
+		$("#video").css("background-color", "chocolate");
+		}, function(){
+			$("#video").css("border-style", "");
+			$("#video").css("border-color", "");
+			$("#video").css("background-color", "");
+		});
+});
+
+
