e64a5ecd0865534455518e1bb8fb81bb9f308f44
serfepe5 <68746993+serfepe5@users.noreply.github.com>
2022-03-13 22:32:29 +0100

    Add files via upload

---
+++ hamburgueseria12&12/js/propio.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+	$("#logojs").hover(function(){
+        $("#logojs").css("height", "100px");
+		$("#logojs").css("width", "100px");
+		}, function(){
+			$("#logojs").css("width", "80px");
+            $("#logojs").css("height", "80px");
+		});
+});
+
+$(document).ready(function(){
+	$("#contacto").hover(function(){
+        $("#contacto").css("font-size", "25px");
+		}, function(){
+			$("#contacto").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#calidad").hover(function(){
+        $("#calidad").css("font-size", "20px");
+		}, function(){
+			$("#calidad").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#reserva").hover(function(){
+        $("#reserva").css("font-size", "25px");
+		}, function(){
+			$("#reserva").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#carta").hover(function(){
+        $("#carta").css("font-size", "25px");
+		}, function(){
+			$("#carta").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#video").hover(function(){
+		$("#video").css("border-style", "solid");
+        $("#video").css("border-color", "chocolate");
+		$("#video").css("background-color", "chocolate");
+		}, function(){
+			$("#video").css("border-style", "");
+			$("#video").css("border-color", "");
+			$("#video").css("background-color", "");
+		});
+});
+
+
