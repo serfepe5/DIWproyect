f7f127433d39ccd62952178f818e4b6d2dc40f2e
serfepe5 <68746993+serfepe5@users.noreply.github.com>
2022-03-13 23:49:50 +0100

    pizzas

---
+++ Pizzas/js/propio.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function(){
+	$("#logojs").hover(function(){
+        $("#logojs").css("height", "100px");
+		$("#logojs").css("width", "200px");
+		}, function(){
+			$("#logojs").css("width", "300px");
+            $("#logojs").css("height", "120px");
+		});
+});
+
+$(document).ready(function(){
+	$("#rejs").hover(function(){
+        $("#rejs").css("font-size", "25px");
+		}, function(){
+			$("#rejs").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#injs").hover(function(){
+        $("#injs").css("font-size", "20px");
+		}, function(){
+			$("#injs").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#conjs").hover(function(){
+        $("#conjs").css("font-size", "25px");
+		}, function(){
+			$("#conjs").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#contjs").hover(function(){
+        $("#contjs").css("font-size", "25px");
+		}, function(){
+			$("#contjs").css("font-size", "");
+		});
+});
+
+$(document).ready(function(){
+	$("#carjs").hover(function(){
+        $("#carjs").css("font-size", "25px");
+		}, function(){
+			$("#carjs").css("font-size", "");
+		});
+});
+
+
+$(document).ready(function(){
+	$("#video").hover(function(){
+		$("#video").css("border-style", "solid");
+        $("#video").css("border-color", "green");
+		$("#video").css("background-color", "green" );
+		}, function(){
+			$("#video").css("border-style", "");
+			$("#video").css("border-color", "");
+			$("#video").css("background-color", "");
+		});
+});
